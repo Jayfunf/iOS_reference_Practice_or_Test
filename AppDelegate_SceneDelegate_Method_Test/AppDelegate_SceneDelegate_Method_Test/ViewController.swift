c199e8de3339197e098b5dc60cf325b746932bd8
Jayfunf <simh3077@gmail.com>
2021-09-05 16:59:54 +0900

    add: AppDelegate_Test

---
+++ AppDelegate_SceneDelegate_Method_Test/AppDelegate_SceneDelegate_Method_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppDelegate_SceneDelegate_Method_Test
+//
+//  Created by Minhyun Cho on 2021/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
